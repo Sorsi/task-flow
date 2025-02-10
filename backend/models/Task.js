2396255b2986ccd6e0b4e287a74ebbf85bcf70c5
Sorsi <orsolya.suto.so@gmail.com>
2025-02-10 13:51:44 +0100

    added default value to task schema

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
   name: { type: String, required: true },
   assignee: { type: String, required: false },
-  added: { type: Date },
+  added: { type: Date, default: Date.now },
   completed: { type: Date },
 });
 
