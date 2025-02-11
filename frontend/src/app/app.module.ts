7c52d5db0312e1f85c2fdc3be0034885d5847cc5
Sorsi <orsolya.suto.so@gmail.com>
2025-02-11 16:46:16 +0100

    new dialog added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// modules
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TaskListComponent } from './tasks/task-list/task-list.component';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material.module';
+// components
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { TaskDialogComponent } from './shared/dialogs/task-dialog.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TaskDialogComponent,
+    TaskFormComponent,
     TaskListComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatDialogModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/shared/dialogs/task-dialog.component.ts
+++ frontend/src/app/shared/dialogs/task-dialog.component.ts
@@ -1,22 +1,39 @@
-import { Component, inject } from '@angular/core';
-import {
-  MAT_DIALOG_DATA,
-  MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
-} from '@angular/material/dialog';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-task-dialog',
   templateUrl: './task-dialog.component.html',
 })
-export class TaskDialogComponent {
+export class TaskDialogComponent implements OnInit {
   readonly dialogRef = inject(MatDialogRef<TaskDialogComponent>);
 
+  form: FormGroup;
+  description: string;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({});
+    this.description = '';
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: [this.description || []],
+      description: [this.description || []],
+    });
+  }
   onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    console.log('SAVE this.form.value ', this.form.value);
+
+    this.dialogRef.close(this.form.value);
+  }
+
+  close(): void {
     this.dialogRef.close();
   }
 }
