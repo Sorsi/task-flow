ebdf93fe0c96209a2d38484f2df2c876d7fe18e1
Sorsi <orsolya.suto.so@gmail.com>
2025-02-11 16:46:00 +0100

    new form component added

---
+++ frontend/src/app/tasks/task-form/task-form.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+})
+export class TaskFormComponent {
+  taskForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    assignee: new FormControl(''),
+    dueDate: new FormControl(null),
+    priority: new FormControl('Medium', Validators.required),
+    completed: new FormControl(false),
+  });
+
+  submitForm() {
+    if (this.taskForm.valid) {
+      console.log('Task submitted: ', this.taskForm.value);
+    }
+  }
+}
